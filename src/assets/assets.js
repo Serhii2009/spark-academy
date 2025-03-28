35298d4260fdcb91dca2e3dcc9a2132be7b9efa9
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2025-03-28 18:08:42 +0200

    added

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -4,6 +4,8 @@ import gmail_icon from './gmail_icon.svg'
 import phone_icon from './phone_icon.svg'
 import logo from './logo.jpg'
 import burger_menu from './burger_menu.svg'
+import main_mobile_img from './main_mobile_img.jpg'
+import lepestok_main from './lepestok_main.svg'
 
 export const assets = {
   facebook_icon,
@@ -12,4 +14,6 @@ export const assets = {
   phone_icon,
   burger_menu,
   logo,
+  main_mobile_img,
+  lepestok_main,
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,34 @@
 import './Home.css'
+import { assets } from '../../assets/assets'
 
 const Home = () => {
-  return <div></div>
+  return (
+    <div className="home">
+      <div className="home-main">
+        <div className="home-text-content">
+          <h1>Якісні знання - основа успіху</h1>
+
+          <p className="description">
+            Мрієте вільно подорожувати, будувати карʼєру в міжнародній компанії
+            або впевнено спілкуватись з людьми з усього світу?
+            <br />
+            Англійська мова - це двері, які відчиняють безмежний простір
+            можливостей для росту та розвитку.
+            <br />А <span>SparkAcademy</span> - це ключ до втілення цих
+            можливостей в життя!
+          </p>
+
+          <button className="home-cta-button">Записатися на курс →</button>
+        </div>
+
+        <img
+          src={assets.lepestok_main}
+          alt="Lepestok"
+          className="home-image-main"
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Home
