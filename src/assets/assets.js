b4ab8a8ebc03d81d6c7064bb882190c301874db4
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2025-03-28 12:37:07 +0200

    added mob, tab for NavBar

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,10 +2,14 @@ import facebook_icon from './facebook_icon.svg'
 import instagram_icon from './instagram_icon.svg'
 import gmail_icon from './gmail_icon.svg'
 import phone_icon from './phone_icon.svg'
+import logo from './logo.jpg'
+import burger_menu from './burger_menu.svg'
 
 export const assets = {
   facebook_icon,
   instagram_icon,
   gmail_icon,
   phone_icon,
+  burger_menu,
+  logo,
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,59 @@
 import './NavBar.css'
 
+import { useState } from 'react'
+import { assets } from '../../assets/assets'
+
 const NavBar = () => {
-  return <div>NavBar</div>
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <img className="logo" src={assets.logo} alt="Spark Language Academy" />
+
+        <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
+          <li>
+            <a href="#">Головна сторінка</a>
+          </li>
+          <li>
+            <a href="#">Курси</a>
+          </li>
+          <li>
+            <a href="#">Пройти тест</a>
+          </li>
+        </ul>
+
+        <div className="navbar-left-side">
+          <button className="contact-btn-mobile">Зв’язатися</button>
+          <button className="contact-btn">Зв’язатися з нами</button>
+
+          <img
+            onClick={() => setMenuOpen(!menuOpen)}
+            src={assets.burger_menu}
+            alt="Menu"
+            className="burger-icon"
+          />
+        </div>
+      </div>
+
+      {/* Модальне меню для мобільної версії */}
+      {menuOpen && (
+        <div className="mobile-menu">
+          <ul>
+            <li>
+              <a href="#">Головна сторінка</a>
+            </li>
+            <li>
+              <a href="#">Курси</a>
+            </li>
+            <li>
+              <a href="#">Пройти тест</a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </nav>
+  )
 }
 
 export default NavBar
